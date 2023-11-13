3cbc26e701141ec4f6db4f8dcd6b6720ee27b531
huantian <davidtianli@gmail.com>
2023-11-12 17:36:57 -0800

    add brick and proteaaudio

--- flake.nix
+++ flake.nix
@@ -10,23 +10,23 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      haskellPackages = pkgs.haskellPackages;
+      ghc = pkgs.haskell.compiler.ghc94;
 
       hsl = pkgs.haskell-language-server.override {
         supportedGhcVersions = [ "94" ];
       };
     in
     {
-      devShell.${system} = haskellPackages.developPackage {
-        root = ./.;
-        returnShellEnv = true;
-        modifier = drv: pkgs.haskell.lib.addBuildTools drv (
-          [
-            hsl
-            haskellPackages.cabal-install
-            haskellPackages.ghcid
-          ]
-        );
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          ghc
+          hsl
+          pkgs.cabal-install
+          pkgs.stack
+
+          pkgs.pkg-config
+          pkgs.pulseaudio
+        ];
       };
     };
 }
